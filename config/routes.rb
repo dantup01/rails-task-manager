fee44d9caa5473caf7ed22b8f42a893742e807a6
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-01-23 13:52:31 +0000

    completed back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'task', to: 'tasks#index'
+  # Read all
+  get '/tasks', to: 'tasks#index'
+  # Create a task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+  # Read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # Update one tast
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
